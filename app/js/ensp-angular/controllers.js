b90ce73f80ec1582caadd2b7b3995e866fc29e4d
foris <evarisfomekong@gmail.com>
2015-11-19 16:43:36 +0100

    formulaire d'ajout des concours realiser En cours creation d'un article

--- app/js/ensp-angular/controllers.js
+++ app/js/ensp-angular/controllers.js
@@ -165,20 +165,21 @@ angular.module('myApp.publication')
  *****************************************************************************************************************/
 angular.module('myApp.concours')
     .controller('FormulaireConcoursCtrl', function($scope,$routeParams,$location,PublicationFactory,$filter) {
-        $scope.new_publication = false;
+        $scope.new_concours = false;
         var id=$routeParams.id || null;
         if(id!=null)// Edition d'une publication
         {
-            $scope.type="Edition de Publication";
+            $scope.type="Edition de Concours";
         }
         else // creation d'une publication
         {
             $scope.type="Nouveau Concours";
         }
-        $scope.save_publication = function(){
-            if($scope.new_publication!=false){
-                PublicationFactory.addPublication($scope.new_publication);
-                $location.path('/liste-publication')
+        $scope.save_concours = function(){
+            if($scope.new_concours!=false){
+               // PublicationFactory.addPublication($scope.new_publication);
+               // $location.path('/liste-publication')
+                console.log($scope.new_concours);
             }else{
                 console.log('veullez remplir les champs');
             }
